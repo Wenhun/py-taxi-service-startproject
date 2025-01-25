52aa3befe5052da5673610236a91c00907cdab9c
Ilia Terentev <wenhun1@gmail.com>
2025-01-25 17:57:14 +0200

    Solution with Debug Off

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-h=gw%9=6&71-p(qo&@!$h3(4b40xktess+*z1a*2d+^r8%eobf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
